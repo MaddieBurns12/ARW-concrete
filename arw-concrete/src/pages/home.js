2ea38996ad973779acf911a1d84691e52f6bfd77
MaddieBurns12 <mburns6023@gmail.com>
2023-01-10 14:57:40 -0500

    starting on home page

--- arw-concrete/src/pages/home.js
+++ arw-concrete/src/pages/home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import LH from '../images/LH.jpg';
+
 
 const Home = () => {
     return(
-        <div className='home'>home</div>
+        <div className='home'>
+            <img src={LH} alt="test" id='home-image'/>
+        </div>
     )
 }
 
-export default Home
+export default Home;

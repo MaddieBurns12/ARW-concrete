8f785c6cb66f769fb772879c8a431df9db9b8e7c
MaddieBurns12 <mburns6023@gmail.com>
2023-01-10 14:45:23 -0500

    nav bar finished

--- arw-concrete/src/components/Navbar.js
+++ arw-concrete/src/components/Navbar.js
@@ -6,16 +6,16 @@ const Navbar = () => {
             <div className='container'>
             <div className='row'>
                 <h3 className='col select-container'>
-                    <a href='./careers' className='page-select'>Careers/Internships</a>
-                </h3>
-                <h3 className='col select-container'>
-                    <a href='./safety' className='page-select'>Safety Page</a>
-                </h3>
-                <h3 className='col select-container'>
                     <a href='./about' className='page-select'>About Us</a>
                 </h3>
                 <h3 className='col select-container'>
                     <a href='./services' className='page-select'>Services</a>
+                </h3>
+                <h3 className='col select-container'>
+                    <a href='./careers' className='page-select'>Careers/Internships</a>
+                </h3>
+                <h3 className='col select-container'>
+                    <a href='./safety' className='page-select'>Safety Page</a>
                 </h3>
                 </div>
             </div>

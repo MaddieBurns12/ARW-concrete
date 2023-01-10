dbe1fc70ded702973b8ccc6a2c29facb09a860b5
MaddieBurns12 <mburns6023@gmail.com>
2023-01-10 15:30:29 -0500

    finished background video formatting

--- arw-concrete/src/pages/home.js
+++ arw-concrete/src/pages/home.js
@@ -4,7 +4,7 @@ import TEST from '../images/TEST.mp4';
 
 const Home = () => {
     return(
-        <div className='container home'>
+        <div className='home'>
             <video autoPlay="autoplay" loop="loop" muted id='home-image'>
                 <source src={TEST} type="video/mp4"/>
             </video>

d7d0f7a0047331bdef701f73e018b9943293e150
MaddieBurns12 <mburns6023@gmail.com>
2023-01-09 20:26:46 -0500

    beginning of nav bar functions

--- arw-concrete/src/App.js
+++ arw-concrete/src/App.js
@@ -3,12 +3,37 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import Logo from './components/logo';
+import Home from './pages/home'
+import About from './pages/about';
+import Careers from './pages/careers';
+import Safety from './pages/safety';
+import Services from './pages/services';
 
 function App() {
+  let component
+  switch (window.location.pathname) {
+    case '/':
+      component = <Home />
+      break
+    case '/about':
+      component = <About />
+      break
+    case '/careers':
+      component = <Careers />
+      break
+    case '/safety':
+      component = <Safety />
+      break
+    case '/services':
+      component = <Services />
+      break
+  
+  }
   return (
     <div>
       <Logo />
       <Navbar />
+      {component}
     </div>
 
   )
